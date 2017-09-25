181b726e1a2a6fa1f9401fe2b3042f049e299d17
HarendraSingh22 <harendrasingh2205@gmail.com>
2017-09-25 22:55:14 +0530

    Now seems good to go

--- A_Card_Game.py
+++ A_Card_Game.py
@@ -4,11 +4,14 @@ t=input()
 while(t>0):
     n=input()
     r=range(1,n+1)
-    while(len(r)!=1):
+    x=len(r)
+    while(x!=1):
         k=[]
-        for i in range(1,len(r),2):
-            k = k.append(r[i])
+        for i in range(1,x,2):
+            k.append(r[i])
         r=k
+        x=len(r)
     t=t-1
     print(r)
+
 
